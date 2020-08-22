c5eaacf640bdb969fa1b3176730f44a1810e4883
Lumm1t <lummit1337@gmail.com>
2020-08-23 00:30:24 +0200

    fix: disable vue warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    'vue/no-v-html': 'off'
   }
 };
